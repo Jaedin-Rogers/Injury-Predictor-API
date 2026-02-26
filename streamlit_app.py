f0e84bedd5719610ce4de728944894762e5f9e50
Jaedin Hernandez-Rogers <jaedinihernandez@gmail.com>
2026-02-26 12:56:35 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,26 @@ if st.button("Predict"):
             "fatigue_score":fatigue_score
         }
     )
+<<<<<<< HEAD
     st.write(res.json())
+=======
+     result = res.json()
+
+        prediction = result["prediction"]
+        prob = result.get("probability", 0)
+
+        st.subheader("Result")
+
+        if prediction == 1:
+            st.error(f"⚠️ High Injury Risk ({prob:.0%})")
+        else:
+            st.success(f"✅ Low Injury Risk ({prob:.0%})")
+
+        st.progress(prob)
+
+        if fatigue_score > 7:
+            st.warning("High fatigue detected")
+
+        if recovery_days < 2:
+            st.warning("Low recovery time detected")
+>>>>>>> 16f4729 (Updated UI)
